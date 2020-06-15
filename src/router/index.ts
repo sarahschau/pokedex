f5f0065ad8078578671c1554c46c899381118fac
ssch <ssch@dmb.at>
2020-06-16 00:52:45 +0200

    Add 404 route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,33 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import Error from '../views/404.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/pokemon/:id',
+        name: 'Pokemon',
+        component: () =>
+            import(/* webpackChunkName: "pokemon" */ '../views/Pokemon.vue')
+    },
+    {
+        path: '*',
+        name: 'Error',
+        component: Error
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes,
+    base: process.env.BASE_URL,
+    mode: 'history'
 });
 
 export default router;
