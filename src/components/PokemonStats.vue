69ad713b5b0e479ac0e329f0ed8bc1b246959f6b
ssch <ssch@dmb.at>
2020-06-16 00:49:59 +0200

    Add stats.

---
+++ src/components/PokemonStats.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pokemon-stats">
+        <h2>Stats</h2>
+        <hr />
+        <div>
+            <div v-for="(stat, idx) in stats" :key="idx">
+                <p class="t-text">{{ stat.stat.name }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'PokemonStats',
+    props: {
+        stats: {
+            type: Array
+        }
+    }
+});
+</script>
