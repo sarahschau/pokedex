0b8c8503e3df98c03bfb0bc30dd933f12fa16f25
ssch <ssch@dmb.at>
2020-06-16 14:52:23 +0200

    Add load more button.

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,23 +1,29 @@
 <template>
-    <table v-if="!loading" class="pokemon-list table table-striped">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(pokemon, idx) in pokemons" :key="idx">
-                <th>{{ idx + 1 }}</th>
-                <td>
-                    <router-link :to="'/pokemon/' + pokemon.name">
-                        <!-- eslint-disable-next-line -->
-                        {{ pokemon.name }}
-                    </router-link>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <table v-if="!loading" class="pokemon-list table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(pokemon, idx) in pokemons" :key="idx">
+                    <th>{{ idx + 1 }}</th>
+                    <td>
+                        <router-link :to="'/pokemon/' + pokemon.name">
+                            <!-- eslint-disable-next-line -->
+                            {{ pokemon.name }}
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <button v-if="!hideLoadButton" class="btn btn-light" @click="loadMore">
+            Load more
+        </button>
+    </div>
 </template>
 
 <script lang="ts">
@@ -32,16 +38,24 @@ export default Vue.extend({
     data(): PokemonListData {
         return {
             loading: true,
-            pokemons: null
+            pokemons: [],
+            offset: 0,
+            hideLoadButton: false
         };
     },
     methods: {
         fetchData() {
             axios
-                .get('https://pokeapi.co/api/v2/pokemon/?limit=964')
+                .get(
+                    `https://pokeapi.co/api/v2/pokemon/?limit=100&offset=${this.offset}`
+                )
                 .then((response: AxiosResponse): void => {
-                    this.pokemons = response.data.results;
+                    this.pokemons = this.pokemons.concat(response.data.results);
+                    if (response.data.results.length < 100) {
+                        this.hideLoadButton = true;
+                    }
                     this.loading = false;
+                    this.offset = this.offset + 100;
                 })
                 .catch((error: AxiosError | undefined): void => {
                     if (error && error.response) {
@@ -51,6 +65,9 @@ export default Vue.extend({
                     }
                     this.loading = false;
                 });
+        },
+        loadMore() {
+            this.fetchData();
         }
     },
     mounted() {
